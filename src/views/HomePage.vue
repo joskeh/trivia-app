ad85993d347d5479a7fcdb2407217f9203bebffc
joskeh <joskeh@live.com>
2024-03-05 09:01:55 -0600

    added score to homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,14 +2,19 @@
 import { onMounted, ref } from 'vue'
 import BaseTitle from '@/components/BaseTitle.vue';
 import useAPI from '@/composables/useAPI'
+import MainScore from '@/components/MainScore.vue';
+
 const { categories, getCategories } = useAPI()
+
 onMounted(async () => {
   await getCategories()
 })
+
+
 </script>
 
 <template>
-   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
+  <BaseTitle>TRIVIA APPLICATION - <MainScore></MainScore> </BaseTitle>
   <div class="grid flex-grow grid-cols-4 gap-12 m-20">
     <RouterLink v-for="category in categories" 
     :key="category.id"
@@ -18,5 +23,7 @@ onMounted(async () => {
     >
       {{  category.name }}
     </RouterLink>
-  </div> 
+  </div>  
+
+ 
 </template>
